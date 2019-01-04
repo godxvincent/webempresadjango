6ffb851ac4217a5bc0e7cee94862756d05173e17
godxvincent <godxvincent@gmail.com>
2019-01-03 21:15:31 -0500

    Adding html pages and static documents

--- webempresarial/webempresarial/settings.py
+++ webempresarial/webempresarial/settings.py
@@ -73,10 +73,20 @@ WSGI_APPLICATION = 'webempresarial.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'localhost:1521/ORCLPDB',
+        'USER': 'RICARDO',
+        'PASSWORD': 'Oracle123',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
@@ -103,7 +113,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
