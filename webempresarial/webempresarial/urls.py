6ab6bd97ec9d3bf36aabe1cb6572f1b295627050
godxvincent <godxvincent@gmail.com>
2019-01-03 21:40:13 -0500

    settings.py, url.py index.html core/views.py were modified this changes were to make posible to see the index page running on the server

--- webempresarial/webempresarial/urls.py
+++ webempresarial/webempresarial/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
+    # Al agregar una vista se dice la url que tendrá, la vista y el nombre del recurso
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
