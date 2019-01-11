8c65a153fb1ca71a16acc1ea45879e71a9f53f25
godxvincent <godxvincent@gmail.com>
2019-01-11 15:47:33 -0500

    It's been created a new urlconfig in app core, base.html was created, and every template was modified to have only the specific content that it has to show

--- webempresarial/core/views.py
+++ webempresarial/core/views.py
@@ -4,4 +4,30 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "core/index.html")
+    indexNavBar = {'index': 'Inicio',
+                   'about': 'Historia',
+                   'services': 'Servicios',
+                   'store': 'Visítanos',
+                   'contact': 'Contacto',
+                   'blog': 'blog'}
+    return render(request, "core/index.html", {'indexNavBar': indexNavBar})
+
+
+def blog(request):
+    return render(request, "core/blog.html")
+
+
+def services(request):
+    return render(request, "core/services.html")
+
+
+def store(request):
+    return render(request, "core/store.html")
+
+
+def contact(request):
+    return render(request, "core/contact.html")
+
+
+def about(request):
+    return render(request, "core/about.html")
--- webempresarial/webempresarial/urls.py
+++ webempresarial/webempresarial/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from core import views
+from django.urls import path, include
+# from core import views
 
 urlpatterns = [
     # Al agregar una vista se dice la url que tendrá, la vista y el nombre del recurso
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    # path('blog/', views.blog, name='blog'),
+    # path('services/', views.services, name='services'),
+    # path('store/', views.store, name='store'),
+    # path('contact/', views.contact, name='contact'),
+    # path('about', views.about, name='about'),
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
