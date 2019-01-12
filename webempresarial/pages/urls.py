fa0e97b04542851b2a0a0db0333c2110c71fa644
godxvincent <godxvincent@gmail.com>
2019-01-12 18:00:39 -0500

    templatetags was created and the urlconfig was changed to show the name of the pages in the url

--- webempresarial/pages/urls.py
+++ webempresarial/pages/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     # https://docs.djangoproject.com/en/2.0/topics/http/urls/#path-converters
-    path('<int:page_id>/', views.pages, name='page'),
+    path('<int:page_id>/<slug:page_slug>/', views.pages, name='page'),
 ]
