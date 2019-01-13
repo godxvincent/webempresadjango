a1325e4ebb109f358d1497e238c189fb186fc611
godxvincent <godxvincent@gmail.com>
2019-01-12 22:42:14 -0500

    The contact app was finished and the form was modified to look like to the html base of the project

--- webempresarial/contact/views.py
+++ webempresarial/contact/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from .forms import ContactForms
 
 # Create your views here.
@@ -6,4 +7,12 @@ from .forms import ContactForms
 
 def contact(request):
     contactForm = ContactForms()
+    if request.method == "POST":
+        contactForm = ContactForms(data=request.POST)
+        if contactForm.is_valid():
+            name = request.POST.get('name', '')
+            email = request.POST.get('email', '')
+            content = request.POST.get('content', '')
+            return redirect(reverse('contact')+"?ok")
+
     return render(request, "contact/contact.html", {'contactForm': contactForm})
